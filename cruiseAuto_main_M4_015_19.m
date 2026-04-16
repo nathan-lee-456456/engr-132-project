ebab963e14999d4d834580bc32d9d8a4f1910905
nathan-lee-456456 <lee5698@purdue.edu>
2026-04-15 21:35:38 -0400

    draft of main

--- cruiseAuto_main_M4_015_19.m
+++ cruiseAuto_main_M4_015_19.m
@@ -1,44 +1,67 @@
-replace this text with your function definition line
+function cruiseAuto_main_M4_015_19()
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ENGR 132 
 % Program Description 
-replace this text with your program description as a comment
+% Coordinates Alpha Version of CruiseAuto algorithm. Imports data, 
+% identifies accel start, steady-state speed, and time constant.
 %
 % Function Call
-replace this text with a comment that states the function call
+% cruiseAuto_main_M4_015_19()
 %
 % Input Arguments
-replace this text with a commented list of the input arguments
+% None
 %
 % Output Arguments
-replace this text with a commented list of the input arguments
+% None
 %
 % Assignment Information
 %   Assignment:     Cruise Auto - Main Function
 %   Version:        M4
-%   Team members:   Aarav Jain, jain925@purdue.edu [repeat for each person]
-%                   Abir Anajpur, aanajpur@purdue.edu [repeat for each person]
-%                   Nathan Lee, lee5698@purdue.edu [repeat for each person]
+%   Team members:   Aarav Jain, jain925@purdue.edu 
+%                   Abir Anajpur, aanajpur@purdue.edu 
+%                   Nathan Lee, lee5698@purdue.edu 
 %                   Ishaan Kedar Khambaswadkar, ikhambas@purdue.edu
 %   Team ID:        015-19
-did you complete the assignment information? delete this line if yes
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% ____________________
 %% INITIALIZATION
-
+data_file = 'Sp26_cruiseAuto_newTire_raw_allTests_data.csv';
 
 %% ____________________
 %% CALCULATIONS
 
+% 1. Import and clean data
+[time_norm, speed_clean, metadata] = sub1_data_handler(data_file);
+
+% 2. Acceleration Start Time
+t_start = cruiseAuto_accel_time_SSS_TT_ikhambas([time_norm, speed_clean]);
+
+% 3. Steady State Speed
+v_ss = cruiseAuto_sub4_steady_state_speeds_SSS_TT_aanajpur(time_norm, speed_clean);
+
+% 4. Time Constant
+tau = cruiseAuto_time_constant_015_19_lee5698(time_norm, speed_clean, t_start);
 
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
 
+fprintf('Vehicle: %s | Tire: %s\n', metadata.vehicle, metadata.tire);
+fprintf('Accel Start: %.2f s\n', t_start);
+fprintf('Steady-State Speed: %.2f m/s\n', v_ss);
+fprintf('Time Constant (Tau): %.4f s\n', tau);
+
+figure;
+plot(time_norm, speed_clean, 'b-', 'LineWidth', 1.5);
+hold on; grid on;
+xline(t_start, 'r--', 'Accel Start');
+xline(t_start + tau, 'g--', 'Tau (63.2%)');
+yline(v_ss, 'k:', 'Steady State');
+title(['Speed Response: ', metadata.vehicle, ' - ', metadata.tire]);
+xlabel('Time (s)');
+ylabel('Speed (m/s)');
 
 %% ____________________
 %% RESULTS
 
-
-
-
+end
