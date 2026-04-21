4dd6f6bc2bd893575c5f7696921b557e630e3671
nathan-lee-456456 <lee5698@purdue.edu>
2026-04-21 00:38:50 -0400

    Update cruiseAuto_main_M4_015_19.m

--- cruiseAuto_main_M4_015_19.m
+++ cruiseAuto_main_M4_015_19.m
@@ -61,6 +61,20 @@ title(['Speed Response: ', char(metadata.vehicle), ' - ', char(metadata.tire)]);
 xlabel('Time (s)');
 ylabel('Speed (m/s)');
 
+% Theoretical model
+t_model = linspace(min(time_vec), max(time_vec), 500); 
+v_model = zeros(size(t_model));
+
+for i = 1:length(t_model)
+    if t_model(i) < t_start 
+        v_model(i) = v_initial; 
+    else
+        v_model(i) = v_initial + (v_ss - v_initial) * (1 - exp(-(t_model(i) - t_start) / tau));
+    end
+end
+plot(time_vec, speed_vec, 'b.'); hold on; % Real-world benchmark data points
+plot(t_model, v_model, 'r-', 'LineWidth', 2); % The theoretical model curve
+
 %% ____________________
 %% RESULTS
 
